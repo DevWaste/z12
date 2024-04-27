04f582d3c1e9a9c3dbf377cdc1d398e782e8d241
DevWaste <167251126+DevWaste@users.noreply.github.com>
2024-04-27 13:38:46 +0300

    Update zadanie_1.py

--- zadanie_1.py
+++ zadanie_1.py
@@ -1,14 +1,12 @@
+# Определение класса Transport
 class Transport:
     def __init__(self, name, max_speed, mileage):
         self.name = name
         self.max_speed = max_speed
         self.mileage = mileage
 
-class Autobus(Transport):
-    pass
-
-# Создание объекта Autobus
-bus = Autobus("Renaul Logan", 180, 12)
+# Создание объекта Transport
+Autobus = Transport("Renault Logan", 180, 12)
 
 # Вывод информации об объекте
-print(f"Название автомобиля: {bus.name} Скорость: {bus.max_speed} Пробег: {bus.mileage}")
+print(f"Название автомобиля: {Autobus.name}, Скорость: {Autobus.max_speed}, Пробег: {Autobus.mileage}")
